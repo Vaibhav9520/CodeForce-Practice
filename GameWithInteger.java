aa65df5d157345f6d13d98bc04c1a4c480304862
Vaibhav Singh <vaibhavsingh01080@gmail.com>
2025-06-17 10:18:23 +0530

    second commit

--- GameWithInteger.java
+++ GameWithInteger.java
@@ -6,15 +6,13 @@ public class GameWithInteger {
         long t = sc.nextLong(); 
 
         while (t-- > 0) {
-            long x = sc.nextLong(); 
+            long n = sc.nextLong(); 
 
-            if (x % 3 == 0) {
-                System.out.println("Second");
-            } else {
+            if ((n+1)%3== 0 || (n-1)%3==0) {
                 System.out.println("First");
+            } else {
+                System.out.println("Second");
             }
         }
-
-        sc.close();
     }
 }
---
+++ Legs.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Legs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            
+            int n = sc.nextInt();
+            int count = n/4;
+            int rem = n%4;
+            int ans = rem/2;
+            int result = count + ans;
+            
+            System.out.println(result);
+        }  
+     }
+}
